e30af91ff5202e1a3c962e8e6e7ed423df15cb99
tianzhen <1159161598@qq.com>
2023-02-14 23:10:44 +0800

    第二次提交

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -15,6 +15,6 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 
-git push -f git@github.com:woxing-create/blogs.git master:gh-pages
+git push -f git@github.com:XXX/XXX.git master:gh-pages
 
 cd -
